f23e8bc16b3391175f4357e0367992370c7b617a
Henrique Schmeller <henrique_schmeller@hotmail.com>
2020-06-07 22:04:44 -0300

    criação de rota de listagem de itens

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,20 @@
 import express from 'express';
+import knex from './database/connection'
 
 const routes = express.Router()
 
-routes.get('/', (req, res) => {
-  res.json({message: 'Hello World!'})
+routes.get('/items', async (req, res) => {
+  const items = await knex('items').select('*')
+  const serializedItems = items.map(item => {
+    return {
+      id: item.id,
+      title: item.title,
+      image_url: `http://localhost:3333/uploads/${item.image}`
+
+    }
+  })
+
+  return res.json(serializedItems)
 })
 
 export default routes
