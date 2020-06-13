d7146744d18a9c5af1352d75f284da896d52cb55
henriquesml <henrique_schmeller@hotmail.com>
2020-06-13 19:25:07 -0300

    adição frontend

---
+++ frontend/src/components/Header.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function Header(){
+  return (
+    <header>
+      <h1>Ecoleta</h1>
+    </header>
+  );
+}
